757a44ef3d1ffbf0e62bc473904543823ae9c58b
L-David-T <davidlez2mgk@gmail.com>
2021-11-17 21:19:53 -0600

    menu

--- examen_2/menu.cpp
+++ examen_2/menu.cpp
@@ -5,75 +5,72 @@ using namespace std;
 int main()
 {
     int operacion;
-    do {
-        int opcion ;
-    float lado1, lado2, lado3, lado4, radio, diametro, area, perimetro;
-    cout << "Seleccione su figura";
-    cout << "\n1. Triangulo";
-    cout << "\n2. Cuadrado";
-    cout << "\n3. Circulo";
-    cout << ":  ";
-    cin >> opcion;
-    switch (opcion)
+    do
     {
-    case 1:
-        cout << "Ingrese el lado 1 : ";
-        cin >> lado1;
-        cout << "Ingrese el lado 2 : ";
-        cin >> lado2;
-        cout << "Ingrese el lado 3 : ";
-        cin >> lado3;
-        if (lado1 <=0 || lado2<=0 || lado3<=0)
+        int opcion;
+        float lado1, lado2, lado3, lado4, radio, diametro, area, perimetro;
+        cout << "Seleccione su figura";
+        cout << "\n1. Triangulo";
+        cout << "\n2. Cuadrado";
+        cout << "\n3. Circulo";
+        cout << ":  ";
+        cin >> opcion;
+        switch (opcion)
         {
-            cout << "Favor de ingresar valores validos";
-        }
-        else
-        {
-            area = (lado1 * lado2) / 2;
-            perimetro = lado1 + lado2 + lado3;
-            cout << "El area del triangulo es : " << area << endl;
-            cout << "El perimetro del triangulo es : " << perimetro << endl;
-        }
-        // {
-        //     cout << "entre aqui";
-        //     cout << "El area del triangulo es : " << (lado1 * lado2 * lado3) / 2;
-        //     cout << "El perimetro del triangulo es : " << lado1 + lado2 + lado3;
-        // }
-        break;
-    case 2:
-        cout << "Ingrese el lado 1";
-        cin >> lado1;
-        if (lado1 <= 0)
-        {
-            cout << "Favor de ingresar valores validos";
-        }
-        else
-        {
-            cout << "El area del cuadrado es : " << lado1 * lado1;
-            cout << "El perimetro del cuadrado es : " << lado1 * 4;
-        }
-        break;
-    case 3:
-        cout << "Ingrese el radio";
-        cin >> radio;
-        cout << "Ingrese el diametro";
-        cin >> diametro;
-        if (diametro <= 0 || radio <= 0) 
-        {
-            cout << "Favor de ingresar valores validos";
-        }
-        else
-        {
-            cout << "El area del circulo es : " << 3.14 * radio * radio;
-            cout << "El perimetro del circulo es : " << 2 * 3.14 * radio;
-        }
+        case 1:
+            cout << "Ingrese el lado 1 : ";
+            cin >> lado1;
+            cout << "Ingrese el lado 2 : ";
+            cin >> lado2;
+            cout << "Ingrese el lado 3 : ";
+            cin >> lado3;
+            if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0)
+            {
+                cout << "Favor de ingresar valores validos";
+            }
+            else
+            {
+                area = (lado1 * lado2) / 2;
+                perimetro = lado1 + lado2 + lado3;
+                cout << "El area del triangulo es : " << area << endl;
+                cout << "El perimetro del triangulo es : " << perimetro << endl;
+            }
 
-        break;
-    default:
-        cout << "Opcion no valida";
-    }
-    cout << "\nDesea hacer otra operacion ? (1. Si 2. No)";
-    cin >> operacion;
+            break;
+        case 2:
+            cout << "Ingrese el lado 1";
+            cin >> lado1;
+            if (lado1 <= 0)
+            {
+                cout << "Favor de ingresar valores validos";
+            }
+            else
+            {
+                cout << "El area del cuadrado es : " << lado1 * lado1;
+                cout << "El perimetro del cuadrado es : " << lado1 * 4;
+            }
+            break;
+        case 3:
+            cout << "Ingrese el radio";
+            cin >> radio;
+            cout << "Ingrese el diametro";
+            cin >> diametro;
+            if (diametro <= 0 || radio <= 0)
+            {
+                cout << "Favor de ingresar valores validos";
+            }
+            else
+            {
+                cout << "El area del circulo es : " << 3.14 * radio * radio;
+                cout << "El perimetro del circulo es : " << 2 * 3.14 * radio;
+            }
+
+            break;
+        default:
+            cout << "Opcion no valida";
+        }
+        cout << "\nDesea hacer otra operacion ? (1. Si 2. No)";
+        cin >> operacion;
     } while (operacion == 1);
     return 0;
 }
