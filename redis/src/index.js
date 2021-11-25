f773be1f4395cb2f871d305bd581e7646e3e9d51
L-David-T <davidlez2mgk@gmail.com>
2021-11-24 21:22:20 -0600

    using promisify from util

--- redis/src/index.js
+++ redis/src/index.js
@@ -4,38 +4,55 @@ const axios = require('axios');
 const app = express();
 const responseTime = require('response-time');
 const redis = require('redis');
-const {Promisify} = require('utils')
+const { promisify } = require('util');
 //#endregion
 const client = redis.createClient({
   host: '127.0.0.1',
   port: 6379,
 });
-// #region Express configuration
+// #region  configuration
 app.use(responseTime());
-
+const ASYNC_GET = promisify(client.get).bind(client);
+const ASYNC_SET = promisify(client.set).bind(client);
 //#endregion
 
 //#region Express routes
 app.get('/character', async (req, res) => {
-  client.set('character',async (err, reply) => {
-    if (reply) {
-      return res.json(JSON.parse(reply));
-    }
-    const response =await   axios.get(
+  try {
+    // respoonse form redis
+    const reply = ASYNC_GET('chararter');
+    if (reply) return res.json(JSON.parse(reply));
+
+    //response from api
+    const response = await axios.get(
       'https://rickandmortyapi.com/api/character'
     );
 
-    client.set(
-      'character',
-      JSON.stringify(response.data.results),
-      (err, reply) => {
-        if (err) console.log(err);
-        console.log(reply);
+    await ASYNC_SET('character', JSON.stringify(response.data));
+    res.json(response.data);
+    
+  } catch (err) {
+    console.log(err);
+  }
+  // client.set('character',async (err, reply) => {
+  //   if (reply) {
+  //     return res.json(JSON.parse(reply));
+  //   }
+  //   const response =await   axios.get(
+  //     'https://rickandmortyapi.com/api/character'
+  //   );
 
-        res.json(response.data);
-      }
-    );
-  });
+  //   client.set(
+  //     'character',
+  //     JSON.stringify(response.data.results),
+  //     (err, reply) => {
+  //       if (err) console.log(err);
+  //       res.json(response.data);
+  //       console.log(reply);
+
+  //     }
+  //   );
+  // });
 });
 //#endregion
 
